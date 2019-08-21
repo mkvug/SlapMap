e74d85880999898ed0abf96afd18ed0cc82bdac0
Chris Neigh <chris.neigh@gmail.com>
2019-08-21 07:44:43 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <GlobalHeader/>
+    <Map/>
+    <GlobalFooter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+import Map from './components/Map.vue'
+import GlobalFooter from './components/GlobalFooter.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    GlobalHeader,
+    Map,
+    GlobalFooter
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <img src="@/assets/slapmap.svg"><h1>SlapMap</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'GlobalHeader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+header {
+    position: fixed;
+    top: 0;
+    left: 20px;
+    z-index: 10;
+    img {
+      width: 40px;
+      position: relative;
+      top: 10px;
+    }
+    h1 {
+      font-size: 30px;
+      display: inline-block;
+    }
+}
+</style>
